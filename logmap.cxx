9fb65a8cb4f6fdc79b08dd4a0d6a80f5551a1bea
Kratzradio <maria_kersten@yahoo.de>
2015-11-08 19:23:02 +0100

    hw2 aktuell

--- logmap.cxx
+++ logmap.cxx
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -7,17 +8,26 @@ int main(){
 	double x;
 	int Nskip = 100; //Number of iterations to skip
 	int Nend  = 200; //Number of total iterations
+	ofstream out("output.dat");
 
 	for(double r=0; r <= 4; r += 0.001){
 	   x=x0;
-	   for(int i=1; i <= Nskip; i++)
-		   x = r*x*(1-x);
-	   for(int i=Nskip+1; i <= Nend; i++){
-	   		   x = r*x*(1-x);
-	   		   cout << r << "\t" << x << endl;
+
+	double p[Nend-Nskip];
+
+	   for(int i=1; i <= Nskip; i++) {
+		  	x = r*x*(1.0-x);
+	   }	//iteration steps skipped
+	   for(int i=Nskip+1; i <= Nend; i++) {
+	   		x = r*x*(1.0-x);	//those we actually want
+			cout << r << "\t" << x << endl;
+			p[i-Nskip-1] = x;	//x stored into array
    	   }
+		for(int i = 0; i<=(Nend-Nskip-1); i++)
+		out << r << "\t" << p[i] << endl;
+		//for each r, array into output file
 	}
 
-
+	out.close();
 	return 0;
 }
